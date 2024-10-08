a91fbf23f82e2be3ebab9ce0228795559eaf2c28
Andrew Henry <ajhenry@github.com>
2024-10-08 00:08:47 -0400

    feat: sliders

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -50,21 +50,85 @@ export default function NewReview() {
     console.log(data);
   };
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
+  const RatingSlider = ({
+    form,
+    name,
+    low,
+    high,
+    label,
+    description,
+  }: {
+    form: ReturnType<typeof useForm<z.infer<typeof formSchema>>>;
+    name: keyof z.infer<typeof formSchema>;
+    low: string;
+    high: string;
+    label: string;
+    description: string;
+  }) => {
+    return (
+      <FormItem>
+        <FormLabel>{label}</FormLabel>
+        <FormDescription>{description}</FormDescription>
+        <div>
+          <Slider
+            defaultValue={[form.getValues(name)]}
+            onValueChange={(val) => form.setValue(name, val[0])}
+            max={100}
+            min={0}
+            step={5}
+          />
+          <h2 className="text-sm flex justify-between mt-1">
+            <span>{low}</span>
+            <span>{high}</span>
+          </h2>
         </div>
-      </div>
+        <div>
+          <Editor />
+        </div>
+      </FormItem>
+    );
+  };
+
+  return (
+    <div className="flex-1 w-full flex flex-col space-y-8">
       <div className="w-full">
         <SwitchSearch />
       </div>
       <div className="w-full">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <RatingSlider
+              form={form}
+              name="travel"
+              low="Too Short"
+              high="Too Long"
+              label="Travel"
+              description="How far does the switch travel?"
+            />
+            <RatingSlider
+              form={form}
+              name="weight"
+              low="Too Light"
+              high="Too Heavy"
+              label="Weight"
+              description="How heavy is the switch?"
+            />
+            <RatingSlider
+              form={form}
+              name="sound"
+              low="Too Quiet"
+              high="Too Loud"
+              label="Sound"
+              description="How loud is the switch?"
+            />
+            <RatingSlider
+              form={form}
+              name="typing"
+              low="Bad"
+              high="Great"
+              label="Typing"
+              description="How does the switch feel for typing?"
+            />
             <FormItem>
               <FormLabel>Feel</FormLabel>
               <FormDescription>

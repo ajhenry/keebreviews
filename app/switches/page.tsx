b3dc42e583385cfc3ab9d28522587155cf2e7923
Andrew Henry <andrew@ajhenry.dev>
2024-10-18 22:48:14 -0400

    fix: change order of switch type column

--- app/switches/page.tsx
+++ app/switches/page.tsx
@@ -40,6 +40,14 @@ type Comparator = (a: Row, b: Row) => number;
 
 function getComparator(sortColumn: string): Comparator {
   switch (sortColumn) {
+    case "type":
+      return (a, b) => {
+        return a.spec[sortColumn] === b.spec[sortColumn]
+          ? 0
+          : a.spec[sortColumn] > b.spec[sortColumn]
+            ? 1
+            : -1;
+      };
     case "friendlyName":
       return (a, b) => {
         return a[sortColumn].localeCompare(b[sortColumn]);
@@ -63,6 +71,40 @@ export default function CommonFeatures() {
   const columns = useMemo(() => {
     const data: Column<Row, SummaryRow>[] = [
       {
+        key: "type",
+        name: "Type",
+        sortable: true,
+        renderHeaderCell: (props) => {
+          return (
+            <Button variant="ghost" className="w-full">
+              Type
+              {props.sortDirection === "ASC" ? (
+                <CaretUpIcon className="ml-2 h-4 w-4" />
+              ) : props.sortDirection === "DESC" ? (
+                <CaretDownIcon className="ml-2 h-4 w-4" />
+              ) : (
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+              )}
+            </Button>
+          );
+        },
+        renderCell: (props) => {
+          return (
+            <Badge
+              variant={
+                props.row.spec.type === "linear"
+                  ? "default"
+                  : props.row.spec.type === "tactile"
+                    ? "secondary"
+                    : "destructive"
+              }
+            >
+              {props.row.spec.type}
+            </Badge>
+          );
+        },
+      },
+      {
         key: "friendlyName",
         name: "Name",
         frozen: false,
@@ -79,88 +121,6 @@ export default function CommonFeatures() {
                 <CaretSortIcon className="ml-2 h-4 w-4" />
               )}
             </Button>
-          );
-        },
-      },
-      {
-        key: "type",
-        name: "Type",
-        sortable: false,
-        renderHeaderCell: (props) => {
-          return (
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="ghost"
-                  // onClick={() =>
-                  //   column.toggleSorting(column.getIsSorted() === "asc")
-                  // }
-                >
-                  Type
-                  <CaretSortIcon className="ml-2 h-4 w-4" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-64">
-                <div className="grid gap-4">
-                  <div className="space-y-2">
-                    <h4 className="font-medium leading-none">Type</h4>
-                    <p className="text-sm text-muted-foreground">
-                      Set the dimensions for the layer.
-                    </p>
-                  </div>
-                  <div className="grid gap-2">
-                    {["linear", "tactile", "clicky"].map((switchType) => {
-                      return (
-                        <div className="items-top flex space-x-2">
-                          <Checkbox
-                            id={switchType}
-                            checked={selectedSwitchTypes.includes(switchType)}
-                            onCheckedChange={(checked) => {
-                              console.log(selectedSwitchTypes);
-                              if (checked) {
-                                setSelectedSwitchTypes([
-                                  ...selectedSwitchTypes,
-                                  switchType,
-                                ]);
-                              } else {
-                                setSelectedSwitchTypes(
-                                  selectedSwitchTypes.filter(
-                                    (type) => type !== switchType
-                                  )
-                                );
-                              }
-                            }}
-                          />
-                          <div className="grid gap-1.5 leading-none">
-                            <label
-                              htmlFor={switchType}
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 capitalize"
-                            >
-                              {switchType}
-                            </label>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </PopoverContent>
-            </Popover>
-          );
-        },
-        renderCell: (props) => {
-          return (
-            <Badge
-              variant={
-                props.row.spec.type === "linear"
-                  ? "default"
-                  : props.row.spec.type === "tactile"
-                    ? "secondary"
-                    : "destructive"
-              }
-            >
-              {props.row.spec.type}
-            </Badge>
           );
         },
       },

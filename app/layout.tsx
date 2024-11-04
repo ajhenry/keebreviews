c6b7114478f09ed42cb58ae493b7a4254d32acec
Andrew Henry <andrew@ajhenry.dev>
2024-11-03 22:57:39 -0500

    fix: import analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+      <Analytics />
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
